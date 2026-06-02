cb0b0ebecdbb2796977cd536a96785ffadd11131
Anant Kumar <anantthegreat6@gmail.com>
2026-06-02 16:31:53 +0530

    Day-08 is uploaded successfully

---
+++ Day-08/RepeatedPattern.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class RepeatedPattern {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter height: ");
+        int height = sc.nextInt();
+
+        for (int i = 1; i <= height; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(i + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+    
+}
